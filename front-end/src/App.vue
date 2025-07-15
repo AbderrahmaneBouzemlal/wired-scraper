5fbbef3d66b2679c8ad0469cc14a2e8241a64b8d
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-07-15 16:33:04 +0800

    did the fall back

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -5,31 +5,32 @@ import jsonData from "../../wired_data.json";
 import axios from "axios";
 
 const items = ref([]);
-const next = ref("");
-const previous = ref("");
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/";
 
-async function fetchPosts(url = API_URL) {
-  try {
-    const response = await axios.get(url);
-    items.value = response.data.results;
-    next.value = response.data.next;
-    previous.value = response.data.previous;
-  } catch (error) {
-    console.error("Failed to fetch posts:", error);
-    if (!response.data.results) {
-      items.value = jsonData;
-      console.log(jsonData);
-    }
-  }
-}
-function goToPage(url) {
-  if (url) fetchPosts(url);
-}
+items.value = jsonData;
+// const next = ref("");
+// const previous = ref("");
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/";
 
-onMounted(() => {
-  fetchPosts();
-});
+// async function fetchPosts(url = API_URL) {
+//   try {
+//     const response = await axios.get(url);
+//     items.value = response.data.results;
+//     next.value = response.data.next;
+//     previous.value = response.data.previous;
+//   } catch (error) {
+//     console.error("Failed to fetch posts:", error);
+//     if (!response.data.results) {
+//         items.value = jsonData
+//       }
+//     }
+//   }
+// function goToPage(url) {
+//   if (url) fetchPosts(url);
+// }
+
+// onMounted(() => {
+//   fetchPosts();
+// });
 </script>
 
 <template>
