b20e49cf5155f4926c599cd96fcea0836027604f
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-07-15 15:51:02 +0800

    try to remove the cors

--- back-end/webcrowler/settings.py
+++ back-end/webcrowler/settings.py
@@ -51,12 +51,12 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-'https://wired-scraper.vercel.app',
-'https://wired-scraper-git-main-abderrahmanebouzemlals-projects.vercel.app',
-'https://wired-scraper-ab3iore9z-abderrahmanebouzemlals-projects.vercel.app',
-"http://localhost:3000"
-]
+# CORS_ALLOWED_ORIGINS = [
+# 'https://wired-scraper.vercel.app',
+# 'https://wired-scraper-git-main-abderrahmanebouzemlals-projects.vercel.app',
+# 'https://wired-scraper-ab3iore9z-abderrahmanebouzemlals-projects.vercel.app',
+# "http://localhost:3000"
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_ORIGINS = True 
