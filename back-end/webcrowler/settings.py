38ee1c609d342d9eb4ebddc3fa6fdb160bcbb9b1
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-07-15 15:04:53 +0800

    solved an issue

--- back-end/webcrowler/settings.py
+++ back-end/webcrowler/settings.py
@@ -161,17 +161,3 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-class CorsMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        
-        response["Access-Control-Allow-Origin"] = "https://wired-scraper.vercel.app"
-        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-        response["Access-Control-Allow-Credentials"] = "true"
-        
-        return response
